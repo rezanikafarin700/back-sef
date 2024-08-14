97b87bb75e5820f5c182930190243ec2a1c0927c
rezanikafarin <rezanikafarin100@gmail.com>
2024-08-14 17:17:19 +0330

    create manual Validation and change message to farsi

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Str;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -30,20 +30,53 @@ class UserController extends Controller
     {
         //
     }
-    public function store(StoreRequest $request)
+    public function store(Request $request)
     {
-        $user = User::create([
-            'name' => $request->name,
-            'mobile' => $request->mobile,
-            'type' => $request->type,
-            'city' => $request->city,
-            'address' => $request->address,
-            'email' => $request->email,
-            // 'avatar' => $request->avatar,
-            'password' => bcrypt($request->password),
-            'api_token' => Str::random(100),
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|min:2|max:100',
+            'mobile' => 'required|string|min:11|max:11',
+            'type' => 'required',
+            'city' => 'required|string|min:2|max:50',
+            'address' => 'required|string|min:2',
+            'email' => 'required|string|email|unique:users,email',
+            'password' => 'required|string|min:6|max:50',
+            'avatar' => 'nullable',
+
         ]);
-        return response($user, 201);
+
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'errors' => $validator->messages()
+            ], 422);
+        } else {
+
+            $user = User::create([
+                'name' => $request->name,
+                'mobile' => $request->mobile,
+                'type' => $request->type,
+                'city' => $request->city,
+                'address' => $request->address,
+                'email' => $request->email,
+                'avatar' => $request->avatar,
+                'password' => bcrypt($request->password),
+                'api_token' => Str::random(100),
+            ]);
+        }
+
+        if ($user) {
+            return response()->json([
+                'status' => 201,
+                'message' => 'User Created Successfully'
+            ], 201);
+        } else {
+            return response()->json([
+                'status' => 500,
+                'message' => 'خطا در سرور رخ داده'
+            ], 500);
+        }
+        // return response($user, 201);
     }
 
 
